b70dbf866b409026c2dbd4e132e1c915b0d938e9
Matt <Matt@Mateis-MacBook-Pro.local>
2020-11-19 17:24:08 +0100

    this should fix the image path issue, which isnt allowing images to load on the proper link

--- collection.js
+++ collection.js
@@ -5,7 +5,7 @@ const collection = [
       category: ['Adventure', 'Drama'],
       link: 'https://www.youtube.com/watch?v=HddkucqSzSM',
       description:  "A man living a dull life dreams up romantic and action-filled scenarios in order to escape from monotony. When his own job is threatened, he sets out on a real romantic and action-packed journey.",
-      image: '/photos/movies-walter-12272013-superJumbo.jpg'
+      image: './photos/movies-walter-12272013-superJumbo.jpg'
     },
   
     {
@@ -14,7 +14,7 @@ const collection = [
         category: ['Sci-Fi', 'Drama'],
         link: 'https://www.youtube.com/watch?v=vXf3gVYXlHg',
         description:  "In 2092 the last mortal human on Earth reflects on his long past and thinks about the possible lives he might have led, based on once choice he did or did not make as a child.",
-        image: '/photos/mr-nobody-review-jared-leto.jpg'
+        image: './photos/mr-nobody-review-jared-leto.jpg'
 
       },
 
@@ -24,7 +24,7 @@ const collection = [
         category: ['Adventure', 'Anime', 'Fantasy'],
         link: 'https://www.youtube.com/watch?v=ByXuk9QqQkk',
         description:  "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
-        image: '/photos/c66f29f6e575486aa40db56441fa503d.jpg'
+        image: './photos/c66f29f6e575486aa40db56441fa503d.jpg'
       },
 
       {
@@ -33,7 +33,7 @@ const collection = [
         category: ['Indie', 'Drama', 'Romance'],
         link: 'https://www.youtube.com/watch?v=PaZVjZEFkRs"',
         description:  "Will Hunting, a genius in mathematics, solves all the difficult mathematical problems. When he faces an emotional crisis, he takes help from psychiatrist Dr Sean Maguireto, who helps him recover.",
-        image: '/photos/Robin-Williams.jpg'
+        image: './photos/Robin-Williams.jpg'
       },
       {
         title: 'The Wolf of Wallstreet',
@@ -41,7 +41,7 @@ const collection = [
         category: ['Comedy', 'Drama'],
         link: 'https://www.youtube.com/watch?v=iszwuX1AK6A',
         description:  "Introduced to life in the fast lane through stockbroking, Jordan Belfort takes a hit after a Wall Street crash. He teams up with Donnie Azoff, cheating his way to the top as his relationships slide.",
-        image: '/photos/wolf-of-wall-street.jpg'
+        image: './photos/wolf-of-wall-street.jpg'
       },
       {
         title: 'Donnie Darko',
@@ -49,7 +49,7 @@ const collection = [
         category: ['Thriller', 'Drama', 'Indie'],
         link: 'https://www.youtube.com/watch?v=bzLn8sYeM9o',
         description:  "Donnie Darko, an awkward teenager, befriends Frank, a figure in a bunny costume only he can see, who informs Donnie that the world will end in 28 days, 6 hours, 42 minutes, and 12 seconds.",
-        image: '/photos/Donnie-Darko-does-it-hold-up-gq.jpg'
+        image: './photos/Donnie-Darko-does-it-hold-up-gq.jpg'
       },
       {
         title: 'Hacksaw Ridge',
@@ -57,7 +57,7 @@ const collection = [
         category: ['War', 'Drama', 'Historical'],
         link: 'https://www.youtube.com/watch?v=s2-1hz1juBI',
         description:  "The true story of Pfc. Desmond T. Doss, who won the Congressional Medal of Honor for his bravery, selflessness and compassion after he risked his life, during WWII -- without firing a shot -- to save 75 men in the Battle of Okinawa.",
-        image: '/photos/Hacksaw-ridge-Netflix-1-810x456.jpg'
+        image: './photos/Hacksaw-ridge-Netflix-1-810x456.jpg'
       },
       {
         title: 'Silence',
@@ -65,7 +65,7 @@ const collection = [
         category: ['Drama', 'Historical'],
         link: 'https://www.youtube.com/watch?v=IqrgxZLd_gE',
         description:  "Rodrigues and Garupe, two Catholic missionaries, travel to Japan in search of their missing mentor, Ferreira, who is believed to have been promoting Catholicism by going against the law.",
-        image: '/photos/silence-01456.jpg'
+        image: './photos/silence-01456.jpg'
       },
       {
         title: 'Parasite',
@@ -73,7 +73,7 @@ const collection = [
         category: ['Thriller', 'Drama'],
         link: 'https://www.youtube.com/watch?v=isOGD_7hNIY',
         description:  "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
-        image: '/photos/Brody-Parasite.jpg'
+        image: './photos/Brody-Parasite.jpg'
       },
       {
         title: 'Into the Wild',
@@ -81,6 +81,6 @@ const collection = [
         category: ['Adventure', 'Drama', 'Indie'],
         link: 'https://www.youtube.com/watch?v=XZG1FzyB8DI',
         description:  "Christopher McCandless, a young graduate, decides to renounce all his possessions and hitchhike across America. During his journey, he encounters several situations which change him as a person.",
-        image: '/photos/into the wild.jpg'
+        image: './photos/into the wild.jpg'
       }
   ];
